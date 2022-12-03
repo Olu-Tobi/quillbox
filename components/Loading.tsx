e884892adbbba7864e011a17567c810b6b2d181d
unknown <tobidaramola77@gmail.com>
2022-12-03 11:09:15 +0100

    app 5.0

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,4 +1,8 @@
+import styled from 'styled-components'
 
+const Image = styled.img`
+
+`
 
 
 
@@ -6,10 +10,10 @@ const Loading = () => {
   return (
     <div style={{display: 'grid', placeItems:'center', height:'100vh'}}>
         <div >
-            <img src='https://firebasestorage.googleapis.com/v0/b/shop-c8953.appspot.com/o/Group%202.png?alt=media&token=5c1f3160-c5a9-4440-aa91-304eceb4adca'
+            <Image src='https://firebasestorage.googleapis.com/v0/b/shop-c8953.appspot.com/o/Group%202.png?alt=media&token=5c1f3160-c5a9-4440-aa91-304eceb4adca'
             height={150}
             style={{marginBottom: 10}}
-            
+            alt=''
             />
 
           
